8c21e5b442d692b6315a32adf149b3efb79abaf1
kkihyeon <rlgus5126@gmail.com>
2024-07-24 11:53:56 +0900

    주사위 수정

--- app.py
+++ app.py
@@ -2,11 +2,13 @@ import streamlit as st
 import random 
 
 def show() :
-  st.header('주사위 게임', divider='rainbow')
-  clicked = st.button('주사위던지기', type='primary')
-  st.write(clicked)
-  if clicked :
-    n = random.randint(1, 6)
-    st.image(f'./img/{n}.png')
+    st.header('주사위 게임', divider='rainbow')
+    clicked = st.button('주사위던지기', type='primary')
+    clear = st.button('주사위지우기')
+    if clicked :
+        n = random.randint(1, 6)
+        st.image(f'./img/{n}.png')
+    if clear:
+        del st.image()
 
 show()
