71cbb13ea76e79581a03e6e32c0cd29a16d06351
kkihyeon <rlgus5126@gmail.com>
2024-07-24 11:45:38 +0900

    주사위 수정

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import random
 def show() :
   st.header('주사위 게임', divider='rainbow')
   clicked = st.button('주사위던지기', type='primary')
+  st.write(clicked)
   if clicked :
     n = random.randint(1, 6)
     st.image(f'./img/{n}.png')
